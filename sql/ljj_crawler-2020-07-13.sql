ff5fa94dcc6e3f3feb708ac464ad34f8fe854152
刘晋军 <jinjun.liu@fengjr.com>
2020-07-15 21:15:32 +0800

    sql 文件的更新

--- sql/ljj_crawler-2020-07-13.sql
+++ sql/ljj_crawler-2020-07-13.sql
@@ -25,14 +25,14 @@ CREATE TABLE `extract_info`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `task_id` int(11) NOT NULL COMMENT '解析所属任务的主键id',
   `parent_id` int(11) NULL DEFAULT NULL COMMENT '如果为子解析时，其父解析的主键id信息',
-  `field_name` varchar(255) CHARACTER  NULL DEFAULT NULL COMMENT '解析出的信息的字段名称',
+  `field_name` varchar(255) NULL DEFAULT NULL COMMENT '解析出的信息的字段名称',
   `extract_type` int(11) NOT NULL COMMENT '解析方式 0：css，1：xpath，2：正则，3：js',
-  `extract_attr` varchar(255) CHARACTER  NULL DEFAULT NULL COMMENT '选择属性时设置的值。',
-  `extract_param` varchar(255) CHARACTER  NOT NULL COMMENT '提供给解析器的解析参数',
+  `extract_attr` varchar(255) NULL DEFAULT NULL COMMENT '选择属性时设置的值。',
+  `extract_param` varchar(255) NOT NULL COMMENT '提供给解析器的解析参数',
   `result_type` int(255) NULL DEFAULT NULL COMMENT '解析返回类型 0：string，1：array，2：node',
-  `save_type` varchar(255) CHARACTER  NULL DEFAULT NULL COMMENT '保存方式',
+  `save_type` varchar(255) NULL DEFAULT NULL COMMENT '保存方式',
   `have_child` int(11) NULL DEFAULT NULL COMMENT '是否包含子解析 0：不包含，1：包含',
-  `array_range` varchar(255) CHARACTER  NULL DEFAULT NULL COMMENT '如果是array的时候，设置舍弃的行数eg：3-8，代表第一行第二行不要了，最后的8行不要了',
+  `array_range` varchar(255) NULL DEFAULT NULL COMMENT '如果是array的时候，设置舍弃的行数eg：3-8，代表第一行第二行不要了，最后的8行不要了',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
@@ -56,8 +56,8 @@ INSERT INTO `extract_info` VALUES (10, 2, 5, 'author', 0, NULL, 'span.s5', 0, NU
 DROP TABLE IF EXISTS `task_info`;
 CREATE TABLE `task_info`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `name` varchar(255) CHARACTER  NULL DEFAULT NULL COMMENT '爬虫任务名称',
-  `start_url` varchar(255) CHARACTER  NULL DEFAULT NULL COMMENT '爬虫任务起始url',
+  `name` varchar(255)  NULL DEFAULT NULL COMMENT '爬虫任务名称',
+  `start_url` varchar(255)  NULL DEFAULT NULL COMMENT '爬虫任务起始url',
   `have_rule` int(11) NULL DEFAULT NULL COMMENT 'starturl 是否含有规则',
   `status` int(11) NULL DEFAULT NULL COMMENT '任务运行状态',
   PRIMARY KEY (`id`) USING BTREE
@@ -76,9 +76,9 @@ DROP TABLE IF EXISTS `task_rule`;
 CREATE TABLE `task_rule`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `task_id` int(11) NOT NULL COMMENT '对应所属任务的主键id',
-  `param_name` varchar(255) CHARACTER  NULL DEFAULT NULL COMMENT '对应url中规则部分的参数名称',
+  `param_name` varchar(255)  NULL DEFAULT NULL COMMENT '对应url中规则部分的参数名称',
   `rule_type` int(11) NULL DEFAULT NULL COMMENT '对应param_name的规则类型，0：递增',
-  `rule_param` varchar(255) CHARACTER  NULL DEFAULT NULL COMMENT '用于生成规则的参数',
+  `rule_param` varchar(255)  NULL DEFAULT NULL COMMENT '用于生成规则的参数',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 

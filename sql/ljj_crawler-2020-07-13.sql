8998e471fcc1149ecb1d901ab4a394fb12340b27
刘晋军 <jinjun.liu@fengjr.com>
2020-07-15 21:49:15 +0800

    SQL数据的更新配置

--- sql/ljj_crawler-2020-07-13.sql
+++ sql/ljj_crawler-2020-07-13.sql
@@ -11,10 +11,10 @@
  Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 13/07/2020 16:06:06
+ Date: 15/07/2020 21:48:33
 */
 
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
@@ -25,16 +25,16 @@ CREATE TABLE `extract_info`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `task_id` int(11) NOT NULL COMMENT '解析所属任务的主键id',
   `parent_id` int(11) NULL DEFAULT NULL COMMENT '如果为子解析时，其父解析的主键id信息',
-  `field_name` varchar(255) NULL DEFAULT NULL COMMENT '解析出的信息的字段名称',
+  `field_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '解析出的信息的字段名称',
   `extract_type` int(11) NOT NULL COMMENT '解析方式 0：css，1：xpath，2：正则，3：js',
-  `extract_attr` varchar(255) NULL DEFAULT NULL COMMENT '选择属性时设置的值。',
-  `extract_param` varchar(255) NOT NULL COMMENT '提供给解析器的解析参数',
+  `extract_attr` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '选择属性时设置的值。',
+  `extract_param` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '提供给解析器的解析参数',
   `result_type` int(255) NULL DEFAULT NULL COMMENT '解析返回类型 0：string，1：array，2：node',
-  `save_type` varchar(255) NULL DEFAULT NULL COMMENT '保存方式',
+  `save_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '保存方式',
   `have_child` int(11) NULL DEFAULT NULL COMMENT '是否包含子解析 0：不包含，1：包含',
-  `array_range` varchar(255) NULL DEFAULT NULL COMMENT '如果是array的时候，设置舍弃的行数eg：3-8，代表第一行第二行不要了，最后的8行不要了',
+  `array_range` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '如果是array的时候，设置舍弃的行数eg：3-8，代表第一行第二行不要了，最后的8行不要了',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of extract_info
@@ -49,6 +49,47 @@ INSERT INTO `extract_info` VALUES (7, 2, 5, 'bookUrl', 0, 'href', 'span.s2 > a',
 INSERT INTO `extract_info` VALUES (8, 2, 5, 'latestChapter', 0, NULL, 'span.s3 > a', 0, NULL, NULL, NULL);
 INSERT INTO `extract_info` VALUES (9, 2, 5, 'latestChapterUrl', 0, 'href', 'span.s3 > a', 0, NULL, NULL, NULL);
 INSERT INTO `extract_info` VALUES (10, 2, 5, 'author', 0, NULL, 'span.s5', 0, NULL, NULL, NULL);
+INSERT INTO `extract_info` VALUES (11, 3, NULL, 'intro', 6, NULL, 'http://www.cgbchina.com.cn/Info/11610216', 0, NULL, 1, NULL);
+INSERT INTO `extract_info` VALUES (12, 3, 11, 'intro', 1, NULL, 'body > div.main > div > div.textContent', 0, NULL, 0, NULL);
+
+-- ----------------------------
+-- Table structure for extract_result_type
+-- ----------------------------
+DROP TABLE IF EXISTS `extract_result_type`;
+CREATE TABLE `extract_result_type`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '解析返回的内容类型',
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '解析返回的内容类型的解释',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of extract_result_type
+-- ----------------------------
+INSERT INTO `extract_result_type` VALUES (1, 'STRING', '解析后的内容为一个字符串');
+INSERT INTO `extract_result_type` VALUES (2, 'ARRAY', '解析后返回的内容为一个数组');
+INSERT INTO `extract_result_type` VALUES (3, 'LINK', '解析后返回的内容为一个链接');
+
+-- ----------------------------
+-- Table structure for extract_type
+-- ----------------------------
+DROP TABLE IF EXISTS `extract_type`;
+CREATE TABLE `extract_type`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '页面类型',
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '关于数据的解释',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of extract_type
+-- ----------------------------
+INSERT INTO `extract_type` VALUES (1, 'HTML', 'response内容为HTML静态页面信息');
+INSERT INTO `extract_type` VALUES (2, 'STRING', 'response内容为纯字符串信息，需要单独进行解析');
+INSERT INTO `extract_type` VALUES (3, 'JSON', 'response内容为JSON 数据（部分数据有call_back，这里可以考虑call_back的处理方式）');
+INSERT INTO `extract_type` VALUES (4, 'FILE', '返回内容为一个文件类型（包含文件夹和文件）');
+INSERT INTO `extract_type` VALUES (5, 'STATIC', '传入的固定内容直接入库');
+INSERT INTO `extract_type` VALUES (6, 'LINK', '传入的解析param为一个链接');
 
 -- ----------------------------
 -- Table structure for task_info
@@ -56,18 +97,19 @@ INSERT INTO `extract_info` VALUES (10, 2, 5, 'author', 0, NULL, 'span.s5', 0, NU
 DROP TABLE IF EXISTS `task_info`;
 CREATE TABLE `task_info`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `name` varchar(255)  NULL DEFAULT NULL COMMENT '爬虫任务名称',
-  `start_url` varchar(255)  NULL DEFAULT NULL COMMENT '爬虫任务起始url',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '爬虫任务名称',
+  `start_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '爬虫任务起始url',
   `have_rule` int(11) NULL DEFAULT NULL COMMENT 'starturl 是否含有规则',
   `status` int(11) NULL DEFAULT NULL COMMENT '任务运行状态',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of task_info
 -- ----------------------------
 INSERT INTO `task_info` VALUES (1, '沧州银行 网点信息抓取', 'http://www.czccb.cn/news/2010927/n7847650.html', 0, 0);
 INSERT INTO `task_info` VALUES (2, '新笔趣阁 玄幻小说抓取', 'http://www.xbiquge.la/fenlei/1_{page}.html', 1, 0);
+INSERT INTO `task_info` VALUES (3, '广发银行基本信息抓取', NULL, 0, NULL);
 
 -- ----------------------------
 -- Table structure for task_rule
@@ -76,11 +118,11 @@ DROP TABLE IF EXISTS `task_rule`;
 CREATE TABLE `task_rule`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `task_id` int(11) NOT NULL COMMENT '对应所属任务的主键id',
-  `param_name` varchar(255)  NULL DEFAULT NULL COMMENT '对应url中规则部分的参数名称',
+  `param_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '对应url中规则部分的参数名称',
   `rule_type` int(11) NULL DEFAULT NULL COMMENT '对应param_name的规则类型，0：递增',
-  `rule_param` varchar(255)  NULL DEFAULT NULL COMMENT '用于生成规则的参数',
+  `rule_param` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '用于生成规则的参数',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of task_rule

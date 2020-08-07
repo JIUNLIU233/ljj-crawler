a10fe6224feba5be0df90448765f77f4269c026f
JIUN·LIU <liujinjun12@outlook.com>
2020-08-07 14:00:10 +0800

    kafka 数据管道 的基本实现

--- ljj-crawler-core/src/main/java/com/ljj/crawler/core/scheduler/impl/KafkaScheduler.java
+++ ljj-crawler-core/src/main/java/com/ljj/crawler/core/scheduler/impl/KafkaScheduler.java
@@ -1,5 +1,6 @@
 package com.ljj.crawler.core.scheduler.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.ljj.crawler.common.constant.KafkaGroupKey;
 import com.ljj.crawler.core.Task;
@@ -9,8 +10,11 @@ import com.ljj.crawler.core.scheduler.Scheduler;
 import com.ljj.crawler.webspider.http.Request;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -21,6 +25,11 @@ import java.util.concurrent.LinkedBlockingQueue;
  * 以kafka 作为一个调度中心，
  * 从kafka的角度来说 所有信息使用同一个topic，使用不同的groupid即可实现
  * 难处就在于，如何维护这个offset。如果任务处理失败，怎么进行处理
+ * <p>
+ * 通过kafka 和本地阻塞队列达成一个中转过程即可。
+ * <p>
+ * 从push 的角度来说，可以预先设置是谁来接收，以及数据类型
+ * <p>
  * Create by JIUN·LIU
  * Create time 2020/8/7
  **/
@@ -28,6 +37,11 @@ import java.util.concurrent.LinkedBlockingQueue;
 @Component
 public class KafkaScheduler implements Scheduler {
 
+
+    @Value("${scheduler.kafka.topic}")
+    private String kafkaTopic;
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
 
     @Resource
     private KafkaListenerEndpointRegistry registry;
@@ -42,7 +56,7 @@ public class KafkaScheduler implements Scheduler {
 
     @Override
     public void pushTask(Task taskInfo) {
-
+        pushToKafka(KafkaGroupKey.taskKey, taskInfo, KafkaGroupKey.taskKey);
     }
 
     /**
@@ -57,26 +71,23 @@ public class KafkaScheduler implements Scheduler {
 
     @KafkaListener(id = KafkaGroupKey.taskKey, topics = "${scheduler.kafka.topic}", groupId = KafkaGroupKey.taskKey, autoStartup = "false")
     public void pollTaskFromKafka(ConsumerRecord<String, String> record) {
-        String value = record.value();
-        long offset = record.offset();
-        log.info("kafka consumer record.offset={},record.value={}", offset, value);
-        JSONObject jsonObject = JSONObject.parseObject(value);
-        String string = jsonObject.getString("dataType");
-        JSONObject data = jsonObject.getJSONObject("data");
-        if (KafkaGroupKey.taskKey.equalsIgnoreCase(string)) {
+        log.debug("receive from kafka  >>> offset={},value={}", record.offset(), record.value());
+        String receive = getReceive(record);
+        String data = getData(record);
+        if (KafkaGroupKey.taskKey.equalsIgnoreCase(receive)) {
+            TaskInfo taskInfo = JSONObject.parseObject(data, TaskInfo.class);
             try {
-                taskInfos.put(data.toJavaObject(TaskInfo.class));
+                taskInfos.put(taskInfo);
             } catch (InterruptedException e) {
-                log.error("kafka poll taskInfo to local error >>> e:", e);
+                log.error("kafka push taskInfo to local error >>> e:", e);
             }
         }
-
     }
 
 
     @Override
     public void pushRequest(Task request) {
-
+        pushToKafka(KafkaGroupKey.downloadKey, request, KafkaGroupKey.downloadKey);
     }
 
     /**
@@ -91,12 +102,27 @@ public class KafkaScheduler implements Scheduler {
 
     @KafkaListener(id = KafkaGroupKey.downloadKey, topics = "${scheduler.kafka.topic}", groupId = KafkaGroupKey.downloadKey, autoStartup = "false")
     public void pollRequestFromKafka(ConsumerRecord<String, String> record) {
+        log.debug("receive from kafka  >>> offset={},value={}", record.offset(), record.value());
 
+        String receive = getReceive(record);
+        String data = getData(record);
+        if (KafkaGroupKey.downloadKey.equalsIgnoreCase(receive)) {
+            Request request = JSONObject.parseObject(data, Request.class);
+            try {
+                requests.put(request);
+            } catch (InterruptedException e) {
+                log.error("kafka push request to local error >>> e:", e);
+            }
+        }
     }
 
     @Override
     public void pushExtract(Task task) {
-
+        if (task instanceof TaskInfo) {
+            pushToKafka(KafkaGroupKey.extractKey, task, KafkaGroupKey.taskKey);
+        } else if (task instanceof ExtractInfo) {
+            pushToKafka(KafkaGroupKey.extractKey, task, KafkaGroupKey.extractKey);
+        }
     }
 
     /**
@@ -111,12 +137,31 @@ public class KafkaScheduler implements Scheduler {
 
     @KafkaListener(id = KafkaGroupKey.extractKey, topics = "${scheduler.kafka.topic}", groupId = KafkaGroupKey.extractKey, autoStartup = "false")
     public void pollExtractFromKafka(ConsumerRecord<String, String> record) {
+        log.debug("receive from kafka  >>> offset={},value={}", record.offset(), record.value());
 
+        String receive = getReceive(record);
+        String data = getData(record);
+        String dataType = getDataType(record);
+        if (KafkaGroupKey.extractKey.equalsIgnoreCase(receive)) {
+            Task ex = null;
+
+            if (KafkaGroupKey.taskKey.equalsIgnoreCase(dataType)) {
+                ex = JSONObject.parseObject(data, TaskInfo.class);
+            } else if (KafkaGroupKey.extractKey.equalsIgnoreCase(dataType)) {
+                ex = JSONObject.parseObject(data, ExtractInfo.class);
+            }
+            if (ex != null)
+                try {
+                    extractInfos.put(ex);
+                } catch (InterruptedException e) {
+                    log.error("kafka push extract to local error >>> e:", e);
+                }
+        }
     }
 
     @Override
     public void pushData(Task extractInfo) {
-
+        pushToKafka(KafkaGroupKey.dataKey, extractInfo, KafkaGroupKey.dataKey);
     }
 
     @Override
@@ -126,7 +171,45 @@ public class KafkaScheduler implements Scheduler {
 
     @KafkaListener(id = KafkaGroupKey.dataKey, topics = "${scheduler.kafka.topic}", groupId = KafkaGroupKey.dataKey, autoStartup = "false")
     public void pollDataFromKafka(ConsumerRecord<String, String> record) {
-        log.info("kafka consumer record.offset={},record.value={}", record.offset(), record.value());
+        log.debug("receive from kafka  >>> offset={},value={}", record.offset(), record.value());
+
+        String receive = getReceive(record);
+        String data = getData(record);
+        if (KafkaGroupKey.dataKey.equalsIgnoreCase(receive)) {
+            ExtractInfo extractInfo = JSONObject.parseObject(data, ExtractInfo.class);
+            try {
+                dataInfos.put(extractInfo);
+            } catch (InterruptedException e) {
+                log.error("kafka push data to local error >>> e:", e);
+            }
+        }
+    }
+
+
+    public void pushToKafka(String receive, Task data, String dataTyp) {
+        JSONObject kafkaValue = new JSONObject() {{
+            put("receive", receive);
+            put("data", data);
+            put("dataType", dataTyp);
+        }};
+        String dataStr = kafkaValue.toJSONString();
+        log.info("push data to kafka >>> topic={}, value={}", kafkaTopic, dataStr);
+        kafkaTemplate.send(kafkaTopic, dataStr);
+    }
+
+    public String getReceive(ConsumerRecord<String, String> record) {
+        JSONObject jsonObject = JSONObject.parseObject(record.value());
+        return jsonObject.getString("receive");
+    }
+
+    public String getData(ConsumerRecord<String, String> record) {
+        JSONObject jsonObject = JSONObject.parseObject(record.value());
+        return jsonObject.getString("data");
+    }
+
+    public String getDataType(ConsumerRecord<String, String> record) {
+        JSONObject jsonObject = JSONObject.parseObject(record.value());
+        return jsonObject.getString("dataType");
     }
 
 
--- ljj-crawler-endpoint/src/main/java/com/ljj/crawler/EndpointApplication.java
+++ ljj-crawler-endpoint/src/main/java/com/ljj/crawler/EndpointApplication.java
@@ -1,11 +1,17 @@
 package com.ljj.crawler;
 
+import com.ljj.crawler.common.utils.AppContext;
+import com.ljj.crawler.core.scheduler.Scheduler;
+import com.ljj.crawler.core.scheduler.impl.KafkaScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 通过启动参数 sc=kafka 来指定使用什么传递消息数据
+ */
 @SpringBootApplication
 public class EndpointApplication implements ApplicationRunner {
 
@@ -23,6 +29,22 @@ public class EndpointApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // 默认采用本地队列单机运行
-        crawlerInitial.start();
+
+        Scheduler scheduler = null;
+
+        String[] sourceArgs = args.getSourceArgs();
+        for (String sourceArg : sourceArgs) {
+            if (sourceArg != null && sourceArg.contains("sc=")) {
+                String[] split = sourceArg.split("sc=");
+                if ("kafka".equalsIgnoreCase(split[1])) {
+                    KafkaScheduler kafkaScheduler = AppContext.getBean(KafkaScheduler.class);
+                    kafkaScheduler.startListener();
+                    scheduler = kafkaScheduler;
+
+                    break;
+                }
+            }
+        }
+        crawlerInitial.start(scheduler);
     }
 }
